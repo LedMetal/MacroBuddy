bc915951d639124ffc251c3dff2eb898443b9b9c
Abdul Sadiq <abdulsadiq@hotmail.com>
2017-11-13 20:23:47 -0500

    Delete changeModel() function since I will not be attempting to change the ng-model value in tbInput

--- MacroBuddy/JS/angularModule.js
+++ MacroBuddy/JS/angularModule.js
@@ -3,47 +3,9 @@ var myApp = angular.module('myApp', [])
     .controller('mainController', ['$scope', function($scope) {
         $scope.question = 1;
 
-        $scope.changeModel = function() {
-            $scope.question++;
-            switch ($scope.question) {
-                case 1:     // Name
-                    $('#tbInput').attr('ng-model', 'name');
-
-                    break;
-                case 2:     // Gender
-                    $('#tbInput').attr('ng-model', 'gender');
-
-                    break;
-                case 3:     // Age
-                    $('#tbInput').attr('ng-model', 'age');
-
-                    break;
-                case 4:     // Height
-                    $('#tbInput').attr('ng-model', 'height');
-
-                    break;
-                case 5:     // Weight
-                    $('#tbInput').attr('ng-model', 'weight');
-
-                    break;
-                case 6:     // Activity Factor
-                    $('#tbInput').attr('ng-model', 'activityFactor');
-
-                    break;
-                case 7:     // Fitness Goal
-                    $('#tbInput').attr('ng-model', 'fitnessGoal');
-
-                    break;
-                case 8:     // Questions Finished
-                    $('#tbInput').attr('ng-model', '');
-
-                    break;
-            }
-        };
-
         // Go to the next question (onClick function on button)
         $scope.nextQuestion = function() {
-            $scope.changeModel();
+            $scope.question++;
 
             switch ($scope.question) {
                 case 1:     // Name

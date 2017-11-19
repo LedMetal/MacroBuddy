93c37a653c1b29dc2bdf5807aa644dcf8990c6a0
Abdul Sadiq <abdulsadiq@hotmail.com>
2017-11-19 01:59:28 -0500

    Include MacroNutrients Breakdown Pie Chart
    Include Calories Per Gram Horizontal Bar Graph

--- MacroBuddy/JS/results.js
+++ MacroBuddy/JS/results.js
@@ -9,7 +9,10 @@ var userProfile = {
     bmr: 1800,
     tdee: 2900,
     fitnessGoal: "Lose Weight",
-    dca: 2400
+    dca: 2400,
+    macroRatio_protein: 45,
+    macroRatio_carbohydrates: 20,
+    macroRatio_fats: 35
 };
 
 // jQuery document ready function
@@ -34,4 +37,89 @@ $(function() {
     // Display DCA Result
     $('#dailyCaloricAllowance')[0].innerHTML = "<span style='font-size: 20px'><strong>" + userProfile.dca + " </strong></span>";
 
+    Chart.defaults.global.defaultFontColor = "white";
+
+    var ctx_macroBreakdown = $('#macroBreakdown');
+    var macroBreakdownChart = new Chart(ctx_macroBreakdown, {
+        type: "pie",
+        data: {
+            labels: ["Protein", "Carbohydrates", "Fats"],
+            datasets: [{
+                label: "Macronutrient Ratios",
+                data: [userProfile.macroRatio_protein, userProfile.macroRatio_carbohydrates, userProfile.macroRatio_fats],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    display: false,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    display: false,
+                    gridLines: {
+                        color: "black"
+                    }
+                }]
+            }
+        }
+    });
+
+    var ctx_caloriesPerGram = $('#caloriesPerGram');
+    var caloriesPerGramChart = new Chart(ctx_caloriesPerGram, {
+        type: "horizontalBar",
+        data: {
+            labels: ["Protein", "Carbohydrates", "Fats"],
+            datasets: [{
+                label: "Calories Per Gram",
+                data: [4, 4, 9],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: "black"
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: "black"
+                    }
+                }]
+            }
+        }
+    });
 });

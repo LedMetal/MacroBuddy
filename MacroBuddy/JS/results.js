9b4098651f390219a16a6229d6d80388cd116b4e
Abdul Sadiq <abdulsadiq@hotmail.com>
2017-11-18 20:29:09 -0500

    Add row in BMR Results table for the result itself Make BMR Formula row single and dynamic

--- MacroBuddy/JS/results.js
+++ MacroBuddy/JS/results.js
@@ -1,5 +1,22 @@
-﻿var userProfile_JSON = window.localStorage.getItem("userProfile");
+﻿//var userProfile_JSON = window.localStorage.getItem("userProfile");
 
-var userProfile = JSON.parse(userProfile_JSON);
+//var userProfile = JSON.parse(userProfile_JSON);
 
-console.log(userProfile);
+//console.log(userProfile);
+
+var userProfile = {
+    gender: "Female"
+};
+
+// jQuery document ready function
+$(function() {
+    // Display appropriate BMR formula
+    if (userProfile.gender == "Male") {
+        $('#bmrFormula')[0].innerHTML = "66 + (6.23 x <strong>weight (lbs)</strong>) + (12.7 x <strong>height (inch)</strong>) - (6.8 x <strong>age</strong>)";
+
+    } else {
+        $('#bmrFormula')[0].innerHTML = "655 + (4.35 x <strong>weight (lbs)</strong>) + (4.7 x <strong>height (inch)</strong>) - (4.7 x <strong>age</strong>)";
+
+    }
+
+});
